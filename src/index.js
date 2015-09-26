b8a4bb70d10abc7d5e559e717a36ea7950b5c74d
jjohnson338 <jaredj@gmx.com>
2015-09-26 18:49:33 -0400

    Added gulp

--- src/index.js
+++ src/index.js
@@ -6,9 +6,4 @@ app.use(express.static(__dirname+'/../public'));
    res.render('index', {currentTemp: '212', idealTemp: '250'});
  });
 
- var server = app.listen(3000, function () {
-   var host = server.address().address;
-   var port = server.address().port;
-
-   console.log('Example app listening at http://%s:%s', host, port);
- });
+ var server = app.listen(3000);
