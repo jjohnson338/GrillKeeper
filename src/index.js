b8f29d7d97baf4be61f6efe5651ea5d9a18a4e2d
jjohnson338 <jaredj@gmx.com>
2015-09-26 18:51:36 -0400

    Changed value for test

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ var app = express();
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname+'/../public'));
  app.get('/', function(req, res){
-   res.render('index', {currentTemp: '212', idealTemp: '250'});
+   res.render('index', {currentTemp: '211', idealTemp: '250'});
  });
 
  var server = app.listen(3000);
