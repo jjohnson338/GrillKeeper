134f9f542616944a7b76b5bace5f0e98e09a4c6a
jjohnson338 <jaredj@gmx.com>
2015-11-03 17:09:31 -0500

    Initial implementation of DiffVoltageToTemp

--- src/index.js
+++ src/index.js
@@ -8,26 +8,26 @@ var io = require('socket.io')(http);
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname+'/../public'));
  app.get('/', function(req, res){
-   res.render('index', {currentTemp: temperatureController.getActualTemperature(),
-                        targetTemp: temperatureController.getTargetTemperature()});
+   res.render('index', {currentTemp: temperatureController.GetActualTemperature(),
+                        targetTemp: temperatureController.GetTargetTemperature()});
  });
 
 
 //Socket.IO
 io.on('connection', function(socket){
   socket.on('increment', function(){
-    temperatureController.incrementTargetTemperature();
+    temperatureController.IncrementTargetTemperature();
     propagateData();
   });
   socket.on('decrement', function(){
-    temperatureController.decrementTargetTemperature();
+    temperatureController.DecrementTargetTemperature();
     propagateData();
   })
 });
 
 function propagateData(){
-  io.sockets.emit('dataupdate', {currentTemp: temperatureController.getActualTemperature(),
-                                  targetTemp: temperatureController.getTargetTemperature()});
+  io.sockets.emit('dataupdate', {currentTemp: temperatureController.GetActualTemperature(),
+                                  targetTemp: temperatureController.GetTargetTemperature()});
 }
 
 http.listen(3000);
--- src/temperatureController.js
+++ src/temperatureController.js
@@ -1,46 +1,67 @@
+var math = require('mathjs');
+var adcinterface = require('./adcinterface');
+
 var temperatureControlerObject = function() {
 
-  var adcinterface = require('./adcinterface');
 
   var targetTemperature = 225;
   var actualTempurature = 0;
-  var voltageDiff = 0;
 
   var getTargetTemperature = function(){
     return targetTemperature;
   }
 
-  var setTargetTemperature = function(newTargetTemp){
+  var SetTargetTemperature = function(newTargetTemp){
     targetTemperature = newTargetTemp;
   }
-  var incrementTargetTemperature = function() {
+  var IncrementTargetTemperature = function() {
     setTargetTemperature(++targetTemperature);
   }
-  var decrementTargetTemperature = function() {
+  var DecrementTargetTemperature = function() {
     setTargetTemperature(--targetTemperature);
   }
 
-  var getActualTemperature = function(){
+  var GetActualTemperature = function(){
     //Run code to determine actual temperature
     adcinterface.readChannel(0, function(thermValue){
       adcinterface.readChannel(1, function(controlValue){
         console.log('ThermValue =' + thermValue);
         console.log('ControlValue =' + controlValue);
-        var controlVoltage = (controlValue*5)/1023;
-        var thermVoltage = (thermValue*5)/1023;
-        voltageDiff = controlVoltage - thermVoltage;
+        var controlVoltage = (controlValue * 5.0)/1023;
+        var thermVoltage = (thermValue * 5.0)/1023;
+        var voltageDiff = controlVoltage - thermVoltage;
+        actualTempurature = DiffVoltageToTemp(voltageDiff);
       });
     });
-    return voltageDiff;
+    return actualTempurature;
   }
+
+  var DiffVoltageToTemp = function(DiffVoltage){
+        //Constants
+        var ExcitationVoltage = 5.0;
+        var R = 47000; //Resistance of Known Resistors
+        //Stienhart-Hart Coefficients
+        var A = 0.000515942869144762;
+        var B = 0.000172084582161862;
+        var C = 2.38992092042526e-7;
+
+        //Calculate Resistance of Thermistor
+        var ThermistorResistance = (-2*R)*(((DiffVoltage/ExcitationVoltage)+0.5)/((2*(DiffVoltage/ExcitationVoltage))-1));
+
+        //Calculate Temp in Kelvin
+        var TempKelvin = 1/(A+(B*(math.log(ThermistorResistance)))+(C*math.pow(math.log(ThermistorResistance), 3)));
+
+        //Convert Kelvin to Fahrenheit and return it
+        return ((TempKelvin - 273.15)*1.8)+32;
+};
 
   return {
     //Pubic functions
-    getTargetTemperature: getTargetTemperature,
-    setTargetTemperature: setTargetTemperature,
-    incrementTargetTemperature: incrementTargetTemperature,
-    decrementTargetTemperature: decrementTargetTemperature,
-    getActualTemperature: getActualTemperature
+    GetTargetTemperature: GetTargetTemperature,
+    SetTargetTemperature: SetTargetTemperature,
+    IncrementTargetTemperature: IncrementTargetTemperature,
+    DecrementTargetTemperature: DecrementTargetTemperature,
+    GetActualTemperature: GetActualTemperature
   }
 }();
 
