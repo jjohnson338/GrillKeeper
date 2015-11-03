ae160d384e774c63563d2c313b04321a6043114c
jjohnson338 <jaredj@gmx.com>
2015-11-03 18:02:11 -0500

    Round degrees

--- src/temperatureController.js
+++ src/temperatureController.js
@@ -54,7 +54,7 @@ var temperatureControlerObject = function() {
         var TempKelvin = 1/(A+(B*(math.log(ThermistorResistance)))+(C*math.pow(math.log(ThermistorResistance), 3)));
         console.log('Temp K =' + TempKelvin);
         //Convert Kelvin to Fahrenheit and return it
-        return ((TempKelvin - 273.15)*1.8)+32;
+        return math.round(((TempKelvin - 273.15)*1.8)+32);
 };
 
   return {
